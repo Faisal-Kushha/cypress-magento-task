f77bbc0dc8db612ca3e16c48bdffe0dda60e1f31
Faisal <fkushha89@hotmail.com>
2023-10-14 21:54:30 +0300

    New update

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,41 +1,146 @@
 /// <reference types= "cypress" />
 
-describe("template spec", () => {
-  it("passes", () => {
+Cypress.on("uncaught:exception", (err, runnable) => {
+  return false;
+});
+
+// describe("template spec", () => {
+//   it("passes", () => {
+//     cy.visit("https://magento.softwaretestingboard.com/");
+
+//     let itemsToSelect = ["Women", "Men", "Gear"];
+
+//     let RandomIndex = Math.floor(Math.random() * itemsToSelect.length);
+//     cy.contains(itemsToSelect[RandomIndex]).click({ force: true });
+
+//     let RandomItem = Math.floor(Math.random() * 4);
+//     cy.get(".product-items").find(".product-item").eq(RandomItem).click();
+
+//     let RandomSize = Math.floor(Math.random() * 2);
+//     let RandomColor = Math.floor(Math.random() * 2);
+
+//     if (RandomIndex == 0) {
+//       cy.get(".swatch-attribute-options")
+//         .find("div.swatch-option.text")
+//         .eq(RandomSize)
+//         .click();
+
+//       cy.get(".swatch-attribute-options")
+//         .find("div.swatch-option.color")
+//         .eq(RandomColor)
+//         .click();
+
+//       cy.wait(2000);
+
+//       cy.get("#product-addtocart-button").click();
+
+//       cy.wait(1000);
+//     } else {
+//       cy.get(".stock > span")
+//         .invoke("text")
+//         .then((theText) => {
+//           if (theText == "In stock") {
+//             cy.get("#product-addtocart-button").click();
+//           } else {
+//             alert("This item is not in the stock");
+//           }
+//         });
+//     }
+//   });
+// });
+Cypress.Commands.add("Add_items", () => {
+  let RandomItem = Math.floor(Math.random() * 4);
+  cy.get(".product-items").find(".product-item").eq(RandomItem).click();
+
+  let RandomSize = Math.floor(Math.random() * 2);
+
+  cy.get(".swatch-attribute-options")
+    .find("div.swatch-option.text")
+    .eq(RandomSize)
+    .click();
+
+  let RandomColor = Math.floor(Math.random() * 2);
+
+  cy.get(".swatch-attribute-options")
+    .find("div.swatch-option.color")
+    .eq(RandomColor)
+    .click();
+
+  cy.wait(2000);
+
+  cy.get(".stock > span")
+    .invoke("text")
+    .then((theText) => {
+      if (theText == "In stock") {
+        cy.get("#product-addtocart-button").click();
+      } else {
+        alert("This item is not in the stock");
+      }
+    });
+  cy.wait(4000);
+});
+
+describe.skip("Add a random item to the cart", () => {
+  it("Add a random item from Women to the cart", () => {
+    cy.visit("https://magento.softwaretestingboard.com/");
+    cy.get("#ui-id-4").click();
+
+    cy.Add_items();
+  });
+  it("Add a random item from Men to the cart", () => {
+    cy.visit("https://magento.softwaretestingboard.com/");
+    cy.get("#ui-id-5").click();
+
+    cy.Add_items();
+  });
+  it("Add a random item from Gear to the cart", () => {
     cy.visit("https://magento.softwaretestingboard.com/");
 
-    let itemsToSelect = ["Women", "Men"];
+    cy.get("#ui-id-6").click();
 
-    let RandomIndex = Math.floor(Math.random() * itemsToSelect.length);
-    cy.log(RandomIndex + "@@@@@@@@@@@@@@@@@@@@@@@@@");
-    cy.contains(itemsToSelect[RandomIndex]).click({ force: true });
+    let RandomItem = Math.floor(Math.random() * 4);
+    cy.get(".product-items").find(".product-item").eq(RandomItem).click();
 
-    // this is will let cypress to select second item and press on it
-    cy.get(".products-grid.grid").find("div.product-item-info").eq(1).click();
+    cy.wait(2000);
 
-    cy.get(".swatch-attribute-options.clearfix")
-      .find("div.swatch-option.color")
-      .eq(1)
-      .click();
-    cy.get(".swatch-attribute-options.clearfix")
-      .find("div.swatch-option.text")
-      .eq(1)
-      .click();
+    cy.get(".stock > span")
+      .invoke("text")
+      .then((theText) => {
+        if (theText == "In stock") {
+          cy.get("#product-addtocart-button").click();
+        } else {
+          alert("This item is not in the stock");
+        }
+      });
+    cy.get(".page.messages")
+      .invoke("text")
+      .then((text) => {
+        if ((text = "The requested qty is not available")) {
+          cy.log("++++++++++++++++++++++++++++++");
+        }
+      });
+    cy.wait(4000);
+  });
+});
 
-    // now you need to let cypress check if the text is "IN STOCK" then add to cart
-    // other alert msg that you can't add this item since we don't have it in the stock
-    if (
-      cy.get(".stock > span").invoke("text").should("contain", "In stock") ==
-      true
-    ) {
-      cy.get("#product-addtocart-button").click();
-    } else {
-      cy.log("This item is out of the stock");
-    }
+describe("Login page", () => {
+  it("Login with the correct Email and password", () => {
+    cy.visit(
+      "https://magento.softwaretestingboard.com/customer/account/login/referer/aHR0cHM6Ly9tYWdlbnRvLnNvZnR3YXJldGVzdGluZ2JvYXJkLmNvbS9kZWlyZHJlLXJlbGF4ZWQtZml0LWNhcHJpLmh0bWw%2C/"
+    );
+
+    cy.get("#email").type("faisal.kushha@gmail.com");
+    cy.get("#pass").type("abc123???");
+    cy.get("#send2").click();
   });
 
-  it("", () => {
-    /// now you need to let cypress selecet randomly between men or women and then select any random item
-    // maybe the first , second , third , forth , and also for the color must be randomly selected
+  it("login with incorrect Email and password", () => {
+    cy.visit(
+      "https://magento.softwaretestingboard.com/customer/account/login/referer/aHR0cHM6Ly9tYWdlbnRvLnNvZnR3YXJldGVzdGluZ2JvYXJkLmNvbS9kZWlyZHJlLXJlbGF4ZWQtZml0LWNhcHJpLmh0bWw%2C/"
+    );
+
+    cy.get("#email").type("abc@gmail.com");
+    cy.get("#pass").type("abc124");
+    cy.get("#send2").click();
   });
 });
